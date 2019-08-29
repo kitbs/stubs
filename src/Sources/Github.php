b4f03a64c2d7b4fc33cdad2a91760ff3831aef60
dillingham <brian@dillingham.dev>
2019-08-29 12:18:33 -0400

    Unpack github sub directory

--- src/Sources/Github.php
+++ src/Sources/Github.php
@@ -3,26 +3,52 @@
 namespace Stub\Sources;
 
 use ZipArchive;
-use RecursiveIteratorIterator;
 use RecursiveDirectoryIterator;
 
 class Github
 {
     public $path;
 
-    public function __construct($repository)
+    public function __construct($url)
     {
         $this->path = uniqid('stub_');
 
+        $zipPath = "{$this->path}/master.zip";
+
+        $repository = str_replace('https://github.com/', '', $url);
         $url = "https://github.com/$repository/archive/master.zip";
 
-        file_put_contents("{$this->path}/master.zip", file_get_contents($url));
+        mkdir($this->path);
+
+        file_put_contents($zipPath, file_get_contents($url));
+
+        list($username, $project) = explode('/', $repository);
 
         $zip = new ZipArchive;
-        $zip->open("{$this->path}/master.zip");
-        $zip->extractTo($this->path);
+        $zip->open($zipPath);
+
+        // Remove nested folder
+        for ($i = 0; $i < $zip->numFiles; $i++) {
+            $path = $zip->getNameIndex($i);
+            $zip->extractTo($this->path, [$path]);
+            $cleaned = str_replace("$project-master/", '', $path);
+            if ($cleaned) {
+                rename("$this->path/$path", "$this->path/$cleaned");
+            }
+        }
+
         $zip->close();
 
         unlink("{$this->path}/master.zip");
+
+        // remove empty folders from ./repository-master
+        $directories = new RecursiveDirectoryIterator("{$this->path}/$project-master/");
+        $directories->setFlags(RecursiveDirectoryIterator::SKIP_DOTS);
+
+        foreach ($directories as $directory) {
+            rmdir($directory->getPathname());
+        }
+
+        rmdir("{$this->path}/$project-master/");
     }
 }
